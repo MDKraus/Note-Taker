1e867902a9b6e0138b33a1dcf2956a2689512db3
LanceLancealot <maxfield.kraus@gmail.com>
2024-02-13 19:35:33 -0500

    edited app.listen

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.delete('/api/notes/:id', (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
